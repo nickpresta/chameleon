5e16391368b0a4b33d00aee9b63df651a2642111
Nick Presta <nick@nickpresta.ca>
2014-07-10 03:35:38 -0400

    Extracting out handlers

---
+++ handlers/handlers.go
@@ -0,0 +1,82 @@
+package handlers
+
+import (
+	"bytes"
+	"io"
+	"log"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+
+	"github.com/NickPresta/gomeleon/cache"
+	"github.com/NickPresta/gomeleon/config"
+)
+
+func CachedProxyMiddleware(handler http.HandlerFunc, server config.ServerDefinition, c cache.Cacher) http.HandlerFunc {
+	parsedURL, err := url.Parse(server.URL)
+	if err != nil {
+		panic(err)
+	}
+
+	return func(w http.ResponseWriter, r *http.Request) {
+		// Change the host for the request for this server config
+		r.Host = parsedURL.Host
+		r.URL.Host = r.Host
+		r.URL.Scheme = parsedURL.Scheme
+		r.RequestURI = ""
+
+		key := c.Key(r)
+		response := c.Get(key)
+
+		cached := "not cached"
+		if response != nil {
+			cached = "cached"
+		}
+
+		log.Printf("-> Proxying [%v] to %v\n", cached, r.URL)
+
+		// We don't have a cached response yet
+		if response == nil {
+			// Create a recorder, so we can get data out and modify it (if needed)
+			rec := httptest.NewRecorder()
+			handler(rec, r) // Actually call our handler
+
+			c.Put(key, rec)
+
+			copyHeaders(w.Header(), rec.Header())
+			w.WriteHeader(rec.Code)
+			io.Copy(w, rec.Body) // Write out response
+
+			return
+		}
+
+		// Fetch from cache, return that response
+		for k, v := range response.Headers {
+			w.Header().Add(k, v)
+		}
+		w.WriteHeader(response.StatusCode)
+		io.Copy(w, bytes.NewReader(response.Body))
+	}
+}
+
+func copyHeaders(dst, src http.Header) {
+	for k, vv := range src {
+		for _, v := range vv {
+			dst.Add(k, v)
+		}
+	}
+}
+
+func ProxyHandler(w http.ResponseWriter, r *http.Request) {
+	client := &http.Client{}
+	resp, err := client.Do(r)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	defer resp.Body.Close()
+	copyHeaders(w.Header(), resp.Header)
+	w.WriteHeader(resp.StatusCode)
+	io.Copy(w, resp.Body) // Proxy through
+}
--- main.go
+++ main.go
@@ -1,20 +1,17 @@
 package main
 
 import (
-	"bytes"
 	"flag"
 	"fmt"
-	"io"
 	"log"
 	"net/http"
-	"net/http/httptest"
-	"net/url"
 	"os"
 	"path"
 	"sync"
 
 	"github.com/NickPresta/gomeleon/cache"
 	"github.com/NickPresta/gomeleon/config"
+	"github.com/NickPresta/gomeleon/handlers"
 )
 
 var configPath *string
@@ -26,75 +23,6 @@ func init() {
 	}
 	configFile := path.Join(cwd, "config.json")
 	configPath = flag.String("config", configFile, "Full path to configuration file")
-}
-
-func cachedProxyMiddleware(handler http.HandlerFunc, server config.ServerDefinition, c cache.Cacher) http.HandlerFunc {
-	parsedURL, err := url.Parse(server.URL)
-	if err != nil {
-		panic(err)
-	}
-
-	return func(w http.ResponseWriter, r *http.Request) {
-		// Change the host for the request for this server config
-		r.Host = parsedURL.Host
-		r.URL.Host = r.Host
-		r.URL.Scheme = parsedURL.Scheme
-		r.RequestURI = ""
-
-		key := c.Key(r)
-		response := c.Get(key)
-
-		cached := "not cached"
-		if response != nil {
-			cached = "cached"
-		}
-
-		log.Printf("-> Proxying [%v] to %v\n", cached, r.URL)
-
-		// We don't have a cached response yet
-		if response == nil {
-			// Create a recorder, so we can get data out and modify it (if needed)
-			rec := httptest.NewRecorder()
-			handler(rec, r) // Actually call our handler
-
-			c.Put(key, rec)
-
-			copyHeaders(w.Header(), rec.Header())
-			w.WriteHeader(rec.Code)
-			io.Copy(w, rec.Body) // Write out response
-
-			return
-		}
-
-		// Fetch from cache, return that response
-		for k, v := range response.Headers {
-			w.Header().Add(k, v)
-		}
-		w.WriteHeader(response.StatusCode)
-		io.Copy(w, bytes.NewReader(response.Body))
-	}
-}
-
-func copyHeaders(dst, src http.Header) {
-	for k, vv := range src {
-		for _, v := range vv {
-			dst.Add(k, v)
-		}
-	}
-}
-
-func proxyHandler(w http.ResponseWriter, r *http.Request) {
-	client := &http.Client{}
-	resp, err := client.Do(r)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	defer resp.Body.Close()
-	copyHeaders(w.Header(), resp.Header)
-	w.WriteHeader(resp.StatusCode)
-	io.Copy(w, resp.Body) // Proxy through
 }
 
 func main() {
@@ -114,7 +42,7 @@ func main() {
 			cacher := cache.NewDiskCacher(s.DataDirectory)
 
 			mux := http.NewServeMux()
-			mux.HandleFunc("/", cachedProxyMiddleware(proxyHandler, s, cacher))
+			mux.HandleFunc("/", handlers.CachedProxyMiddleware(handlers.ProxyHandler, s, cacher))
 			log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", s.Port), mux))
 		}(server)
 	}
